a1516e8eb3c6c21f019cba11d08578d6b9bf6a87
Samuel Evans-Powell <mail@sevanspowell.net>
2024-05-07 21:35:22 +0800

    Add default template

--- flake.nix
+++ flake.nix
@@ -4,10 +4,12 @@
   outputs = { self, nixpkgs, ... } @ inputs:
   {
     templates = {
-      cross = {
+      counter = {
         path = ./templates/01-counter;
         description = "Simple Haskell web template using the new GHC Javascript backend.";
       };
     };
+
+    defaultTemplate = self.templates.counter;
   };
 }
