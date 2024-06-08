43eba629cd4732e5fe4b3dad9574b4173c31b781
Samuel Evans-Powell <mail@sevanspowell.net>
2024-06-08 16:36:15 +0800

    WIP

--- templates/04-oauth/src/frontend/Main.hs
+++ templates/04-oauth/src/frontend/Main.hs
@@ -13,8 +13,13 @@ import           Miso
 import           Miso.String
 import           Data.Aeson (FromJSON)
 import qualified Data.Aeson as Aeson
+import Data.Maybe (fromMaybe)
+import Data.Either.Combinators (mapLeft)
+import Data.ByteString (ByteString)
 import qualified JavaScript.Web.XMLHttpRequest as XHR
 import GHC.Generics (Generic)
+import GHCJS.Types (JSVal)
+import Language.Javascript.JSaddle ((#), jsg, (!))
 
 -- | JSAddle import
 #ifndef ghcjs_HOST_OS
@@ -24,23 +29,36 @@ import           Network.WebSockets
 #endif
 import           Control.Monad.IO.Class
 import qualified Control.Concurrent as C
+import Network.URI (URI)
+import Data.Proxy ()
+import Servant.API ()
+import Servant.Links ()
 
 -- | TODO: Load config
-oauthBaseUrl = "http://api.example.com:8082"
+oauthBaseUrl = "http://localhost:8082"
 apiBaseUrl = "http://api.example.com:8081"
 
 -- | Type synonym for an application model
-data Model = Init
-           | Authenticated
-           | NotAuthenticated
-           | AuthenticationError MisoString
+data AuthenticationStatus = Init
+                          | Authenticated
+                          | NotAuthenticated
+                          | AuthenticationError MisoString
+  deriving (Eq, Show)
+
+data Model = Model { currentURI :: URI
+                   , authStatus :: AuthenticationStatus
+                   }
   deriving (Eq, Show)
 
 -- | Sum type for application events
 data Action
   = NoOp
   | CheckAuthentication
-  | SetAuthentication (Either MisoString ())
+  | SetAuthentication (Either MisoString AuthenticateResponse)
+  | StartAuthentication
+  | DoAuthRedirect (Either MisoString URI)
+  | HandleURI URI
+  | ChangeURI URI
   deriving (Show, Eq)
 
 #ifndef ghcjs_HOST_OS
@@ -53,52 +71,108 @@ runApp app = app
 
 -- | Entry point for a miso application
 main :: IO ()
-main = runApp $ startApp App {..}
-  where
-    initialAction = CheckAuthentication -- initial action to be executed on application load
-    model  = Init                       -- initial model
-    update = updateModel                -- update function
-    view   = viewModel                  -- view function
-    events = defaultEvents              -- default delegated events
-    subs   = []                         -- empty subscription list
-    mountPoint = Nothing                -- mount point for application (Nothing defaults to 'body')
-    logLevel = Off                      -- used during prerendering to see if the VDOM and DOM are in synch (only used with `miso` function)
+main = runApp $ do
+    currentURI <- getCurrentURI
+    startApp $
+      App { model = Model { currentURI = currentURI, authStatus = Init }
+          , initialAction = CheckAuthentication -- initial action to be executed on application load
+          , update = updateModel
+          , view = viewModel
+          , events = defaultEvents
+          , subs = [ uriSub HandleURI ]
+          , mountPoint = Nothing
+          , logLevel = Off
+          }
 
 -- | Updates model, optionally introduces side effects
 updateModel :: Action -> Model -> Effect Action Model
 updateModel CheckAuthentication m = m <# do
-  SetAuthentication <$> getAuthState
+  SetAuthentication <$> getAuthState (currentURI m)
 updateModel (SetAuthentication result) m =
   case result of
-    Left err -> noEff (AuthenticationError err)
-    Right () -> noEff Authenticated
+    Left err ->
+      (m { authStatus = AuthenticationError err }) <# do
+        pure $ ChangeURI ((currentURI m) { uriQuery = ""} )
+    Right r  ->
+      if not (isLoggedIn r)
+      then do
+        (m { authStatus = NotAuthenticated }) <# do
+          pure $ ChangeURI ((currentURI m) { uriQuery = ""} )
+      else
+        (m { authStatus = Authenticated }) <# do
+          pure $ ChangeURI ((currentURI m) { uriQuery = ""} )
+updateModel (HandleURI u) m = noEff $ m { currentURI = u }
+updateModel (ChangeURI u) m = m <# do
+  pushURI u
+  pure NoOp
+updateModel StartAuthentication m = m <# do
+  (DoAuthRedirect . fmap authorizationRequestUrl) <$> startAuth
+updateModel (DoAuthRedirect result) m =
+  case result of
+    Left err -> noEff $ m { authStatus = AuthenticationError err }
+    Right u  -> m <# do
+      assignURI (toMisoString $ show u)
+      pure NoOp
+updateModel NoOp m = noEff m
+
 
 -- | Constructs a virtual DOM from a model
 viewModel :: Model -> View Action
-viewModel Init = div_ [] [ text "You are being authenticated" ]
-viewModel Authenticated = div_ [] [ text "You are authenticated" ]
-viewModel NotAuthenticated = div_ [] [ text "You are NOT authenticated" ]
-viewModel (AuthenticationError e) = div_ [] [ text $ "There was an error: '" <> e <> "'." ]
+viewModel m =
+  case authStatus m of
+    Init                    -> div_ [] [ text "You are being authenticated" ]
+    Authenticated           -> div_ [] [ text "You are authenticated" ]
+    NotAuthenticated        ->
+      div_ []
+        [ text "You are NOT authenticated"
+        , button_ [ onClick StartAuthentication ] [ text "Sign In" ]
+        ]
+    (AuthenticationError e) -> div_ [] [ text $ "There was an error: '" <> e <> "'." ]
 
 -- foreign import javascript unsafe
 --   "((x) => { x.withCredentials = true; })"
 --   js_setWithCredentials :: JS.XMLHttpRequest -> DOM.DOM ()
 
-getAuthState :: IO (Either MisoString ())
-getAuthState = do
+getAuthState :: URI -> IO (Either MisoString AuthenticateResponse)
+getAuthState pageURI = do
   let
     req = XHR.Request { reqMethod = XHR.POST
-                      , reqURI = pack "http://www.example.com:8082/oauth-agent/login/end"
+                      , reqURI = pack $ oauthBaseUrl <> "/oauth-agent/login/end"
+                      , reqLogin = Nothing
+                      , reqHeaders = [("content-type", "application/json")]
+                      , reqWithCredentials = True
+                      , reqData = XHR.StringData $
+                          pack $ "{ \"pageUrl\": \"" <> show pageURI <> "\" }"
+                      }
+  result <- mkXHRRequestJSON req
+  case result of
+    Left err -> pure $ Left $ "OAuth agent returned error: '" <> err <> "'."
+    Right resp -> pure $ Right resp
+
+startAuth :: IO (Either MisoString AuthRedirect)
+startAuth = do
+  let
+    req = XHR.Request { reqMethod = XHR.POST
+                      , reqURI = pack $ oauthBaseUrl <> "/oauth-agent/login/start"
                       , reqLogin = Nothing
                       , reqHeaders = []
                       , reqWithCredentials = True
                       , reqData = XHR.NoData
                       }
 
-  Just resp <- XHR.contents <$> XHR.xhrByteString req
-  case Aeson.eitherDecodeStrict resp of
-    Left s -> error s
-    Right (j :: AuthenticateResponse) -> pure $ Right ()
+  result <- mkXHRRequestJSON req
+  case result of
+    Left err -> pure $ Left $ "OAuth agent returned error: '" <> err <> "'."
+    Right resp -> pure $ Right resp
+
+data AuthRedirect =
+  -- TODO: authorization -> authentication
+  AuthRedirect { authorizationRequestUrl :: URI
+               }
+  deriving (Eq, Show, Generic)
+
+instance FromJSON AuthRedirect where
+  parseJSON = Aeson.genericParseJSON Aeson.defaultOptions
 
 data AuthenticateResponse
   = AuthenticateResponse { handled :: Bool
@@ -109,3 +183,39 @@ data AuthenticateResponse
 
 instance FromJSON AuthenticateResponse where
   parseJSON = Aeson.genericParseJSON Aeson.defaultOptions
+
+mkXHRRequestJSON :: FromJSON a => XHR.Request -> IO (Either MisoString a)
+mkXHRRequestJSON req = do
+  result <- mkXHRRequest req
+  pure $ case result of
+    Left err ->
+      Left err
+    Right bytes ->
+      mapLeft toMisoString $ Aeson.eitherDecodeStrict bytes
+
+mkXHRRequest :: XHR.Request -> IO (Either MisoString ByteString)
+mkXHRRequest req = do
+  resp <- XHR.xhrByteString req
+  let statusCode = XHR.status resp
+      -- TODO: 400 Should be treated as auth failure, not error message worthy
+      statusFailure = statusCode < 200 || statusCode > 299
+
+  if statusFailure
+  then do
+    let failureMsg =
+          case XHR.contents resp of
+            Nothing  -> "no error message was returned."
+            Just msg -> "error message was: '" <> toMisoString msg <> "'."
+    pure $ Left $
+      "XHR request returned response code "
+        <> toMisoString statusCode <> " " <> failureMsg
+  else
+    pure $ Right $ fromMaybe "" $ XHR.contents resp
+
+getLocation :: JSM JSVal
+getLocation = jsg ("window" :: String) ! ("location" :: String)
+
+assignURI :: MisoString -> JSM ()
+assignURI uri = do
+  _ <- getLocation # ("assign" :: String) $ [uri]
+  pure ()
